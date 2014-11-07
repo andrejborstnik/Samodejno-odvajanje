0cfc9aff8a7474bb072488d378697b01cb775c78
Andrej <andrej-borstnik@hotmail.com>
2014-11-07 09:33:39 +0100

    Več odvodov.

--- odvajanje.hs
+++ odvajanje.hs
@@ -8,28 +8,6 @@ import Control.Applicative
 	-- pure = const
 	-- f <*> g = \t -> (f t) (g t)
 
-data D a = D a a deriving (Show)
-
-constD :: Num a => a -> D a
-constD x = D x 0
-
-idD :: Num a => a -> D a
-idD x = D x 1
-
-instance Num a => Num (D a) where
-	fromInteger x = constD (fromInteger x)
-	(+) (D a a') (D b b') = D (a + b) (a' + b')
-	(-) (D a a') (D b b') = D (a - b) (a' - b')
-	(*) (D a a') (D b b') = D (a * b) (b' * a + a' * b)
-	negate (D a a') = D (negate a) (negate a')
-	signum (D a _) = D (signum a) 0
-	abs (D a a') = D (abs a) (a' * (signum a))
-	
-instance Fractional x => Fractional(D x) where
-	fromRational x = constD (fromRational x)
-	-- (/) (D a a') (D b b') = D (a/b) ((b' * a + a' * b)/(b^2))
-	recip (D x x') = D (recip x) (-x' / sqr x)
-
 instance Num b => Num (a -> b) where
 	fromInteger = pure . fromInteger
 	negate = fmap negate
@@ -40,7 +18,7 @@ instance Num b => Num (a -> b) where
 
 instance Fractional b => Fractional (a -> b) where 
 	fromRational = pure . fromRational
-	recip = fmap recip
+	recip = fmap recip	
 
 instance Floating b => Floating (a -> b) where
 	pi = pure pi
@@ -57,30 +35,49 @@ instance Floating b => Floating (a -> b) where
 	asinh = fmap asinh
 	acosh = fmap acosh
 	atanh = fmap atanh
+	
+data D a = D a (D a) deriving (Show)
 
 infix 0 ><
-(><) :: Num a => (a -> a) -> (a -> a) -> (D a -> D a)
-(f >< f') (D a a') = D (f a) (a' * f' a)
+(><) :: Num a => (a -> a) -> (D a -> D a) -> (D a -> D a)
+(f >< f') a@(D a0 a') = D (f a0) (a' * f' a)
 
 sqr :: Num a => a -> a
 sqr x = x * x
 
+constD :: Num a => a -> D a
+constD x = D x 0
+
+idD :: Num a => a -> D a
+idD x = D x 1
+
+instance Num a => Num (D a) where
+	fromInteger = constD . fromInteger
+	D a a' + D b b' = D (a + b) (a' + b')
+	x@(D x0 x') * y@(D y0 y') = D (x0 * y0) (x' * y + x * y')
+	negate = negate >< -1
+	signum = signum >< 0
+	abs = abs >< signum
+	
+instance Fractional x => Fractional(D x) where
+	fromRational = constD . fromRational
+	recip = recip >< -sqr recip
 
 instance Floating x => Floating (D x) where
-	pi = D pi 0
+	pi = constD pi
 	exp = exp >< exp
 	log = log >< recip
-	sqrt = sqrt >< \x -> recip (2 * sqrt x)
+	sqrt = sqrt >< recip (2 * sqrt)
 	sin = sin >< cos
-	cos = cos >< \x -> - sin x
-	asin = asin >< \x -> recip (sqrt (1 - sqr x))
-	acos = acos >< \x -> - recip (sqrt (1 - sqr x))
-	atan = atan >< \x -> recip (1 + sqr x)
+	cos = cos >< - sin
+	asin = asin >< recip (sqrt (1 - sqr))
+	acos = acos >< - recip (sqrt (1 - sqr))
+	atan = atan >< recip (1 + sqr)
 	sinh = sinh >< cosh
 	cosh = cosh >< sinh
-	asinh = asinh >< \x -> recip (sqrt (1 + sqr x))
-	acosh = acosh >< \x -> recip (sqrt (- 1 + sqr x))
-	atanh = asin >< \x -> recip (1 - sqr x)
+	asinh = asinh >< recip (sqrt (1 + sqr))
+	acosh = acosh >< recip (sqrt (- 1 + sqr))
+	atanh = asin >< recip (1 - sqr)
 	
 	
 	
